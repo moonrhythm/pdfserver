4120a14685cf39a165d363d56c402648a633f3fd
Panot Wongkhot <panotza_ja@hotmail.com>
2022-08-27 14:02:48 +0700

    share headless browser for each connection

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-use std::{
-    convert::Infallible,
-    net::SocketAddr,
-};
+use std::{convert::Infallible, net::SocketAddr};
+use std::sync::Arc;
+
 use bytes::Buf;
+use chromiumoxide::{browser::{Browser, BrowserConfig}, cdp::browser_protocol::page::PrintToPdfParams};
 use futures::StreamExt;
-use chromiumoxide::{
-    browser::{Browser, BrowserConfig},
-    cdp::browser_protocol::page::PrintToPdfParams,
-};
-use hyper::{
-    Body, header, Method, Request, Response, Server,
-    service::{make_service_fn, service_fn},
-};
+use hyper::{Body, header, Method, Request, Response, Server, service::{make_service_fn, service_fn}};
 use serde::{Deserialize, Serialize};
+use tokio::sync;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
@@ -25,27 +19,29 @@ async fn main() -> Result<()> {
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
-    // let (browser, mut handler) = Browser::launch(
-    //     BrowserConfig::builder()
-    //         .build()?
-    // ).await?;
-    // tokio::spawn(async move {
-    //     loop {
-    //         let _ = handler.next().await.unwrap();
-    //     }
-    // });
-
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(handler))
+    let browser_config = BrowserConfig::builder().build()?;
+    let (browser, mut browser_handler) = Browser::launch(browser_config).await?;
+    tokio::spawn(async move {
+        loop {
+            let _ = browser_handler.next().await.unwrap();
+        }
     });
+    let browser = Arc::new(browser);
 
+    let make_svc = make_service_fn(move |_conn| {
+        let browser = Arc::clone(&browser);
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                handler(Arc::clone(&browser), req)
+            }))
+        }
+    });
     let server = Server::bind(&addr)
         .serve(make_svc)
         .with_graceful_shutdown(shutdown_signal());
 
     println!("start pdf server on http://{}", addr);
     server.await?;
-
     Ok(())
 }
 
@@ -59,8 +55,8 @@ struct PdfRequest {
     scale: Option<f64>,
 }
 
-async fn handler(req: Request<Body>) -> Result<Response<Body>> {
-    if req.method() != &Method::POST {
+async fn handler(browser: Arc<Browser>, req: Request<Body>) -> Result<Response<Body>> {
+    if req.method() != Method::POST {
         return Ok(Response::new("PDF Server".into()));
     }
 
@@ -71,8 +67,7 @@ async fn handler(req: Request<Body>) -> Result<Response<Body>> {
         Err(e) => {
             let resp = Response::builder()
                 .status(400)
-                .body(Body::from(format!("invalid request {:?}", e)))
-                .unwrap();
+                .body(Body::from(format!("invalid request {:?}", e)))?;
             return Ok(resp);
         }
     };
@@ -95,42 +90,54 @@ async fn handler(req: Request<Body>) -> Result<Response<Body>> {
         prefer_css_page_size: None,
         transfer_mode: None,
     };
-    let pdf_data = match do_convert_pdf(data.content.as_str(), pdf_params).await {
+    let pdf_data = match do_convert_pdf(browser, data.content, pdf_params).await {
         Ok(r) => r,
         Err(e) => {
             eprintln!("can not convert pdf, err={:?}", e);
             let resp = Response::builder()
                 .status(500)
-                .body(Body::from("can not convert pdf"))
-                .unwrap();
+                .body(Body::from("can not convert pdf"))?;
             return Ok(resp);
         }
     };
 
     let resp = Response::builder()
         .header(header::CONTENT_TYPE, "application/pdf")
-        .body(Body::from(pdf_data))
-        .unwrap();
+        .body(Body::from(pdf_data))?;
     Ok(resp)
 }
 
-async fn do_convert_pdf(content: &str, params: PrintToPdfParams) -> Result<Vec<u8>> {
-    // TODO: global browser ?
-    let browser_config = BrowserConfig::builder()
-        .build()?;
-
-    let (browser, mut handler) = Browser::launch(browser_config).await?;
+async fn do_convert_pdf(browser: Arc<Browser>,
+                        content: String,
+                        params: PrintToPdfParams,
+) -> Result<Vec<u8>> {
+    let (tx, rx) = sync::oneshot::channel();
     tokio::spawn(async move {
-        // loop {
-        let _ = handler.next().await.unwrap();
-        // }
+        let page = match browser.new_page("about:blank").await {
+            Ok(p) => p,
+            Err(e) => {
+                dbg!(e);
+                return;
+            }
+        };
+        match page.set_content(content).await {
+            Ok(p) => p,
+            Err(e) => {
+                let _ = page.close().await;
+                dbg!(e);
+                return;
+            }
+        };
+        let data = match page.pdf(params).await {
+            Ok(p) => p,
+            Err(e) => {
+                dbg!(e);
+                let _ = page.close().await;
+                return;
+            }
+        };
+        let _ = page.close().await;
+        let _ = tx.send(data);
     });
-
-    let page = browser.new_page("about:blank").await?;
-    page.set_content(content).await?;
-
-    match page.pdf(params).await {
-        Ok(data) => Ok(data),
-        Err(e) => Err(e.into())
-    }
+    Ok(rx.await?)
 }
